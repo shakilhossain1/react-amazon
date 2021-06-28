6f890c4ed7b5f0eb3f3e77faf5b13e76fd26c450
Shakil Hossain <sh9743441@gmail.com>
2021-06-28 20:59:36 +0600

    Initialize app and clean up

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h2>Hello world</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD448uMEKYHfMYiXtD3nUJWz9LMrexgCqs",
+    authDomain: "clone-2bfec.firebaseapp.com",
+    projectId: "clone-2bfec",
+    storageBucket: "clone-2bfec.appspot.com",
+    messagingSenderId: "308292252113",
+    appId: "1:308292252113:web:ad629b31dc1005650cb0f0",
+    measurementId: "G-X0RJF6EZTF"
+  };
